dc1323cecf987df345601d0225ad967c996cc0d1
Jeganathan Swaminathan <jegan@tektutor.org>
2018-11-28 15:51:50 +0530

    Integrated BDD Test Cases in CI

--- src/test/java/org/tektutor/AppTest.java
+++ src/test/java/org/tektutor/AppTest.java
@@ -3,14 +3,25 @@ package org.tektutor;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
 public class AppTest {
+	private App app;
+	private String actualResponse;
+@Given("The Hello class works fine")
+public void the_Hello_class_works_fine() {
+	app = new App();
+}
 
-	@Test
-	public void testCase1() {
-		App obj = new App();
-		String expectedOutput = "Hello Maven!";
-		String actualOutput = obj.sayHello();
+@When("I invoke sayHello method")
+public void i_invoke_sayHello_method() {
+	actualResponse = app.sayHello();
+}
 
-		assertEquals ( expectedOutput, actualOutput );
-	}
+@Then("I expect {string}")
+public void i_expect(String expectedResponse) {
+	assertEquals ( expectedResponse, actualResponse );
+}
 }
