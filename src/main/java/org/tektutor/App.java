8a8623caaa69ad1949c70ad7158912b83e0452ce
Jeganathan Swaminathan <mail2jegan@gmail.com>
2020-07-01 05:49:36 +0530

    Fixed merge conflicts.

--- src/main/java/org/tektutor/App.java
+++ src/main/java/org/tektutor/App.java
@@ -8,6 +8,7 @@ public class App {
 
 	public String someLowPriorityNewFeature() {
 		System.out.println ( "Work in Progress ..." );
+		System.out.println ( "Some additional changes done ..." );
 	}
 
 	public static void main ( String args[] ) {		
